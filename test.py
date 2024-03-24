70241ba11697018ca5b497f63ab22958221c37f2
deflatedDbug <subinlebow2@gmail.com>
2024-03-23 20:22:18 -0400

    initial commit

---
+++ test.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import os 
+import tkinter as tk
+from tkinter import filedialog
+
+# Initializing Tkinter root
+root = tk.Tk()
+root.withdraw()
+
+# Prompt the user to select an image file
+image_path = filedialog.askopenfilename(
+    title="Select an Image",
+    filetypes=[("Image files", "*.jpg *.jpeg *.png")]
+)
+
+# Check if a file was selected
+if not image_path: 
+    print("No file selected. Exiting.")
+
+else: 
+    # Initialize the YOLO model with your custom model weights
+
+    model_weights = "runs/detect/train/weights/best.pt"
+
+    model = YOLO(model_weights)
+
+    result = model(image_path, save=True)
+
+
